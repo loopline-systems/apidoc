3ec8cec37dcaaf3b78123f7b8a41d18d142bcf7f
Loopline IT <it-accounts@loopline.systems>
2015-07-21 09:44:42 +0000

    API Documentation Clamps Version feature/11_api-proxy, Revision 6cb110a2deeec552d40b40a7c3a19a8ed095de6f

---
+++ js/api-doc.js
@@ -0,0 +1,19 @@
+jQuery(function($) {
+    $('nav#nav a.action').each(function() {
+        var $navItem = $(this);
+        var $target = $($navItem.attr('data-target')).find('.content-group');
+        $navItem.click(function() {
+            $target.collapse('open');
+        });
+    });
+
+    $('.content-group').on('show.bs.collapse', function() {
+        var $action = $(this);
+        $action.find('[role="requestTrigger"].collapsed').removeClass('collapsed');
+        $action.find('[role="request"]').css('height', 'auto').addClass('in');
+    });
+
+    $('.content-group [role="request"]').on('show.bs.collapse', function(event) {
+        event.stopPropagation();
+    });
+});
